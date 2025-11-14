8d5c507c1da686e671827dc534097f67f2bcfbbc
DESKTOP-68S7AU4\ROWTCEH <nashar.ahmed@selise.ch>
2025-11-14 14:41:08 +0600

    chore: clean up project

--- src/modules/file-manager/components/regular-file-details-sheet/regular-file-details-sheet.tsx
+++
@@ -1,8 +0,0 @@
-import {
-  BaseFileDetailsSheetProps,
-  FileDetailsSheet,
-} from '../file-manager-details-sheet/file-manager-details-sheet';
-
-export const RegularFileDetailsSheet: React.FC<Omit<BaseFileDetailsSheetProps, 'variant'>> = (
-  props
-) => <FileDetailsSheet {...props} variant="default" />;
--- src/modules/file-manager/components/trash/trash-files-details/trash-files-details.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {
-  BaseFileDetailsSheetProps,
-  FileDetailsSheet,
-} from '../../file-manager-details-sheet/file-manager-details-sheet';
-
-export const TrashDetailsSheet: React.FC<Omit<BaseFileDetailsSheetProps, 'variant'>> = (props) => (
-  <FileDetailsSheet {...props} variant="trash" />
-);
